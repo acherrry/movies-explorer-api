ebd925f7ccb23612a8cef5a38fcb0aa7ce20ad70
Alena Chernyadeva <alyonka_chernyadeva@mail.ru>
2022-10-29 02:44:55 +0300

    поправить объект пользователя для редактирования профиля2

--- utils/validation.js
+++ utils/validation.js
@@ -28,8 +28,8 @@ const userLoginCheck = celebrate({
 
 const userEditProfileCheck = celebrate({
   body: Joi.object().keys({
-    email: Joi.string().required().email(),
     name: Joi.string().required().min(2).max(30),
+    email: Joi.string().required().email(),
   }),
 });
 
